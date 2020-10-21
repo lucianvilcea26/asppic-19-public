ebf6fcfa6ee63508ee779dd757084e50fb30dbd0
Ionut Sandric <sandricionut@yahoo.com>
2020-10-21 10:46:34 +0300

    WebGIS modified

--- ASPPIC.Public/ASPPIC.GIS/koop-providers/judete/src/model.js
+++ ASPPIC.Public/ASPPIC.GIS/koop-providers/judete/src/model.js
@@ -1,4 +1,4 @@
-function Model (koop) {}
+function Model(koop) { }
 
 // each model should have a getData() function to fetch the geo data
 // and format it into a geojson
@@ -16,33 +16,26 @@ Model.prototype.getData = async function (req, callback) {
     geojson.metadata.geometryType = 'MultiPolygon';
     geojson.metadata.idField = "f1";
     // geojson.metadata.maxRecordCount = 50;
-    geojson.metadata.fields = [{"name":"f1", type:"Integer"}, {"name":"f2", type:"String"} ];
+    geojson.metadata.fields = [{ "name": "f1", type: "Integer" }, { "name": "f2", type: "String" }];
     // geojson.ttl = _.get(metadataCopy, 'ttl', 10)
-
-    // var fs = require('fs');
-    // fs.writeFileSync("c:/koopsrv/demogis/data/judete.json", JSON.stringify(geojson), (err) =>
-    // {
-    //   if(err)
-    //   console.log("Error write geojson");
-
-    // });
 
     callback(null, geojson)
   }
   catch (err) {
     callback(err, null);
-  } 
+  }
 }
 
 async function query_postgis_judete() {
   // Setup connection
-  var username = username
-  var password = password
-  var host = host
-  var database = database name
+  var username = ""; // Username
+  var password = "" // Password
+  var host = "" //Server name and port
+  var database = "" // Database name
+  //Database connection string
   var conString = "postgres://" + username + ":" + password + "@" + host + "/" + database;
-  // Your Database Connection
 
+  // Create a connection pool
   const { Pool } = require('pg');
   const pool = new Pool({
     connectionString: conString,
@@ -50,20 +43,30 @@ async function query_postgis_judete() {
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
   });
+  // Create the client for PG connection
   const client = await pool.connect();
 
   // Set up your database query to display GeoJSON
+  const query_counties_stats = "select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(t.*)::json)) from public.\"CountiesStats\" as t(county, last_day, last_week, last_month, geom);"
 
-  const counties_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(h.geom)::json As geometry, row_to_json((\"h\".\"objectid\", \"h\".\"Name\")) As properties FROM \"Counties\" As h) As f) As fc"
-  
-  var counties_fc = await client.query(counties_query).then(res => {
-    return res.rows[0].row_to_json;
+
+  var counties_stats_results = await client.query(query_counties_stats).then(res => {
+    return res.rows[0].json_build_object;
   }).catch(err => console.log(err));
 
   client.release();
+  client.end();
+  
+
+  var fs = require('fs');
+  fs.writeFileSync("c:/koopsrv/demogis/data/judete_stats.geojson", JSON.stringify(counties_stats_results), (err) => {
+    if (err)
+      console.log("Error write geojson");
+
+  });
 
   // Wrap features in Feature Collection object
-  return counties_fc;
+  return counties_stats_results;
 }
 
 module.exports = Model
--- ASPPIC.Public/ASPPIC.GIS/koop-providers/spitale/src/model.js
+++ ASPPIC.Public/ASPPIC.GIS/koop-providers/spitale/src/model.js
@@ -3,29 +3,13 @@ function Model(koop) { }
 // each model should have a getData() function to fetch the geo data
 // and format it into a geojson
 Model.prototype.getData = async function (req, callback) {
-  // var geojson = {
-  //   type: 'FeatureCollection',
-  //   features: []
-  // }
 
   try {
     var geojson = await query_postgis();
-    // console.log("Aici: ", geojson);
-    // console.log("Geosjon: ", geojson)
     geojson.metadata = {};
     geojson.metadata.idField = "f1";
-    // geojson.metadata.maxRecordCount = 50;
     geojson.metadata.geometryType = 'Point';
     geojson.metadata.fields = [{"name":"f1", type:"Integer"}, {"name":"f2", type:"String"} ];
-
-
-    var fs = require('fs');
-    fs.writeFileSync("c:/koopsrv/demogis/data/spitale.json", JSON.stringify(geojson), (err) =>
-    {
-      if(err)
-      console.log("Error write geojson");
-
-    });
     
     callback(null, geojson)
   }
@@ -36,10 +20,10 @@ Model.prototype.getData = async function (req, callback) {
 
 async function query_postgis() {
   // Setup connection
-  var username = username
-  var password = password
-  var host = host
-  var database = database name
+  var username = "" // sandbox username
+  var password = "" // read only privileges on our table
+  var host = ""
+  var database = "" // database name
   var conString = "postgres://" + username + ":" + password + "@" + host + "/" + database;
   // Your Database Connection
 
@@ -53,13 +37,22 @@ async function query_postgis() {
   const client = await pool.connect();
 
   // Set up your database query to display GeoJSON
-  var hospitals_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(h.geom)::json As geometry, row_to_json((\"h\".\"objectid\", \"h\".\"HospitalName\")) As properties FROM \"Hospitals\" As h) As f) As fc;";
+  var hospitals_query = "select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(t.*)::json)) from public.\"HospitalsView\" as t(\"HospitalName\",\"Abbreviation\",\"Address\",\"Uat\",\"County\",\"AdditionalData\",\"Category\",\"StatePrivate\",\"Email\",\"Phone\",\"FullLink\",\"GpsRoute\",geom);";
   
   var hospitals_fc = await client.query(hospitals_query).then(res => {
-    return res.rows[0].row_to_json;
-  }).catch(err => console.log(err));
+    return res.rows[0].json_build_object;
+  }).catch(err => console.log("Eroare", err));
 
+  client.end();
   client.release();
+
+  var fs = require('fs');
+  fs.writeFileSync("c:/koopsrv/demogis/data/spitale.geojson", JSON.stringify(hospitals_fc), (err) =>
+  {
+    if(err)
+    console.log("Error write geojson");
+
+  });
 
   // Wrap features in Feature Collection object
   return hospitals_fc;
